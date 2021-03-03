4c04f4f970962311d415b0d76ef42dd6acbd47d3
Henri <52140922+w0lfaton@users.noreply.github.com>
2021-03-03 10:05:01 +0200

    Update to UI and other smaller changes

--- src/com/w0lfaton/ApiResponseController.java
+++ src/com/w0lfaton/ApiResponseController.java
@@ -1,22 +1,35 @@
 package com.w0lfaton;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ApiResponseController {
     private static ScoroAPIService scoroAPIService = new ScoroAPIService();
 
     @FXML
-    private TextField action;
+    private Label action;
+
+    @FXML
+    private Label module;
 
     @FXML
     private TextField objectId;
 
-    public String getResponse(String endpoint) {
-        if (action.getText().trim().equals("view") || action.getText().trim().equals("modify")) {
-            return scoroAPIService.request(endpoint, action.getText().trim(), Integer.parseInt(objectId.getText().trim()));
+    public String getResponse() {
+        if (action.getText().trim().toLowerCase().equals("view") || action.getText().trim().toLowerCase().equals("modify")) {
+            return scoroAPIService.request(module.getText().toLowerCase(), action.getText().trim().toLowerCase(), Integer.parseInt(objectId.getText().trim()));
         } else {
-            return scoroAPIService.request(endpoint, action.getText().trim());
+            return scoroAPIService.request(module.getText().toLowerCase(), action.getText().trim().toLowerCase());
         }
+    }
+
+    public void setAction(String action) {
+        this.action.setText(action);
+    }
+
+    public String setModule(String module) {
+        this.module.setText(module);
+        return module;
     }
 }
